d7a9d3d46d79d7744167d3b35c8f2f537350017f
Jan Quest <mail@quest-it.de>
2017-10-17 15:45:43 +0200

    added a script to add a swapfile of 2G to vultr vps

---
+++ swapfile.sh
@@ -0,0 +1,9 @@
+# This script will add a swapfile to an existing VPS installation of Vultr
+
+dd if=/dev/zero of=/swapfile count=2048 bs=1M
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+
+# Add swapfile to fstab
+sed -i '$a\/swapfile   none    swap    sw  0   0' /etc/fstab
